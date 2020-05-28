5cbbaac757dd846bd7d4a27bb40e51301aa492a0
vtriclot <vtriclot@1671428.no-reply.drupal.org>
2020-05-28 11:36:33 +0200

    Issue #3109492 by vbouchet, ashu1629, omkar06: Implement generalised path pattern match while adding Path constraint

--- modules/memory_limit_policy_path/src/Plugin/MemoryLimitConstraint/Path.php
+++ modules/memory_limit_policy_path/src/Plugin/MemoryLimitConstraint/Path.php
@@ -3,7 +3,12 @@
 namespace Drupal\memory_limit_policy_path\Plugin\MemoryLimitConstraint;
 
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Path\CurrentPathStack;
+use Drupal\Core\Path\PathMatcherInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\memory_limit_policy\MemoryLimitConstraintBase;
+use Drupal\path_alias\AliasManagerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Configure the memory limit based on path.
@@ -14,7 +19,66 @@ use Drupal\memory_limit_policy\MemoryLimitConstraintBase;
  *   description = @Translation("Provide a list of path where the memory limit must be overridden.")
  * )
  */
-class Path extends MemoryLimitConstraintBase {
+class Path extends MemoryLimitConstraintBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * Current path.
+   *
+   * @var \Drupal\Core\Path\CurrentPathStack
+   */
+  protected $currentPath;
+
+  /**
+   * The path matcher.
+   *
+   * @var \Drupal\Core\Path\PathMatcherInterface
+   */
+  protected $pathMatcher;
+
+  /**
+   * An alias manager to find the alias for the current system path.
+   *
+   * @var \Drupal\path_alias\AliasManagerInterface
+   */
+  protected $aliasManager;
+
+  /**
+   * Constructs constraint plugin.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Path\CurrentPathStack $current_path
+   *   The current path.
+   * @param \Drupal\Core\Path\PathMatcherInterface $path_matcher
+   *   The path matcher service.
+   * @param \Drupal\path_alias\AliasManagerInterface $alias_manager
+   *   An alias manager to find the alias for the current system path.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, CurrentPathStack $current_path, PathMatcherInterface $path_matcher, AliasManagerInterface $alias_manager) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+
+    $this->currentPath = $current_path;
+    $this->pathMatcher = $path_matcher;
+    $this->aliasManager = $alias_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('path.current'),
+      $container->get('path.matcher'),
+      $container->get('path_alias.manager')
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -42,7 +106,7 @@ class Path extends MemoryLimitConstraintBase {
   public function getSummary() {
     $paths = explode(PHP_EOL, $this->configuration['paths']);
     array_walk($paths, function (&$path) {
-      $path = rtrim(trim(trim($path, "\r"), '/'), '/');
+      $path = rtrim(trim($path, "\r"), '/');
     });
     $paths = implode(', ', $paths);
 
@@ -53,18 +117,19 @@ class Path extends MemoryLimitConstraintBase {
    * {@inheritdoc}
    */
   public function evaluate() {
-    $current_path = rtrim(trim(\Drupal::service('path.current')->getPath(), '/'), '/');
+    $current_path = trim($this->currentPath->getPath(), "\r");
 
-    foreach (explode(PHP_EOL, $this->configuration['paths']) as $path) {
-      // Remove \r as well if string contains \r\n.
-      $path = rtrim(trim(trim($path, "\r"), '/'), '/');
+    $page_match = $this->pathMatcher->matchPath($current_path, $this->configuration['paths']);
 
-      if (fnmatch($path, $current_path)) {
-        return TRUE;
+    if (!$page_match) {
+      $alias_path = $this->aliasManager->getPathByAlias($current_path);
+
+      if ($current_path !== $alias_path) {
+        $page_match = $this->pathMatcher->matchPath($alias_path, $this->configuration['paths']);
       }
     }
 
-    return parent::evaluate();
+    return $page_match ?? parent::evaluate();
   }
 
 }
